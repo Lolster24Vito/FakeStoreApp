756f02f02e1e385c3faa8fdc9b12de90a5291bff
Lolster24Vito <vitomir.hardi@gmail.com>
2023-06-19 19:39:20 +0200

    Login register menu

--- app/src/main/java/hr/algebra/fakestoreapp/MainHostActivity.kt
+++ app/src/main/java/hr/algebra/fakestoreapp/MainHostActivity.kt
@@ -2,7 +2,12 @@ package hr.algebra.fakestoreapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.core.view.GravityCompat
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
 import hr.algebra.fakestoreapp.databinding.ActivityMainBinding
 import hr.algebra.fakestoreapp.fragment.AboutFragment
 import hr.algebra.fakestoreapp.fragment.ShopItemsFragment
@@ -15,9 +20,25 @@ class MainHostActivity : AppCompatActivity() {
         binding= ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         replaceFragment(ShopItemsFragment())
+        initBottomNavigationMenu()
+        initHamburgerMenu()
+        initNavigation()
+    }
+
+    private fun initNavigation() {
+     //   val navController = Navigation.findNavController(this, R.id.navController)
+      //  NavigationUI.setupWithNavController(binding.navView, navController)
+    }
+
+    private fun initHamburgerMenu() {
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_menu)
+    }
+
+    private fun initBottomNavigationMenu(){
         binding.bottomNavigationView.setOnItemSelectedListener {
             when(it.itemId){
-                 R.id.shopAbout->replaceFragment(AboutFragment())
+                R.id.shopAbout->replaceFragment(AboutFragment())
                 R.id.shopItems->replaceFragment(ShopItemsFragment())
                 R.id.shopMap-> replaceFragment(StoreMapFragment())
             }
@@ -29,5 +50,28 @@ class MainHostActivity : AppCompatActivity() {
         val fragmentTransaction=fragmentManager.beginTransaction()
         fragmentTransaction.replace(R.id.frame_layout,fragment)
         fragmentTransaction.commit()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.navigation_menu,menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            android.R.id.home->{
+                toggleDrawer()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+
+    }
+    private fun toggleDrawer() {
+        if (binding.drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            binding.drawerLayout.closeDrawers()
+        } else {
+            binding.drawerLayout.openDrawer(GravityCompat.START)
+        }
     }
 }
